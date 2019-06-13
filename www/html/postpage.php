25a2c89b0cc874c593eb373754c6333f54a5365b
y-tkbridge <novsleep@outlook.jp>
2019-06-13 12:32:43 +0900

    img upload

--- www/html/postpage.php
+++ www/html/postpage.php
@@ -7,20 +7,26 @@ $app->run();
 
 ?>
 <div class="postpage_container">
-<i class="fas fa-cloud-upload-alt"></i>
-
-<form action="postpage.php" method="POST" enctype="multipart/form-data">
   <div id="app">
-    <input type="file" name="upload_file" @change="change">
-    <div v-if="preview">
-        <img style="width:300px; height:auto;" :src="preview">
+    <form action="postpage.php" method="POST" enctype="multipart/form-data">
+        <div class="preview_area" v-if="preview">
+                <img class="preview_img" :src="preview">
+        </div>
+        <div class="post_area">
+          <div class="post_area">
+          <!-- <i class="icon_size fas fa-cloud-upload-alt"></i> -->
+            <input type="submit" value="ファイルをアップロードする">  
+            <input type="file" name="upload_file" @change="change">  
+          </div>
+
+        </div>
+    
+    </form>
     </div>
 </div>
 
-  <input type="submit" value="ファイルをアップロードする">
-</form>
 
-<!-- 画像プレビューエリア -->
+
 
 <script src="./js/main.js"></script>
 </body>
